69a091d107fb0ad70b84074081f589e4bfe3c00e
adampwright <adam@wrightwaydigitalmedia.com>
2020-07-21 07:02:15 -0700

    typo

--- pre-start.sh
+++ pre-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp /home/wrightway/utilities/Splash.png /usr/share/kodi/media/Splash.png
-cp /home/wrightway/utilities/from-debian-logo.png /usr/share/kodi/addons/skin.estuary/media/from-debian-logo.png
-cp /home/wrightway/utilities/kodi_logo.png /usr/share/kodi/addons/skin.estuary/media/kodi_logo.png
-cp /home/wrightway/utilities/overide.conf /etc/systemd/system/getty@tty1.service.d/override.conf
-cp /home/wrightway/utilities/profile /etc/profile
+cp /target/home/wrightway/utilities/Splash.png /target/usr/share/kodi/media/Splash.png
+cp /target/home/wrightway/utilities/from-debian-logo.png /target/usr/share/kodi/addons/skin.estuary/media/from-debian-logo.png
+cp /target/home/wrightway/utilities/kodi_logo.png /target/usr/share/kodi/addons/skin.estuary/media/kodi_logo.png
+cp /target/home/wrightway/utilities/overide.conf /target/etc/systemd/system/getty@tty1.service.d/override.conf
+cp /target/home/wrightway/utilities/profile /target/etc/profile
 systemctl enable getty@tty1.service
 reboot
