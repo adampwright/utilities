6cff6b0350cfd561645b52ddeb73d728287d2fa1
adampwright <adam@wrightwaydigitalmedia.com>
2020-07-21 08:37:40 -0700

    typo

--- pre-start.sh
+++ pre-start.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 in-target git clone https://github.com/adampwright/utilities.git /home/wrightway/
-in-target cp /home/wrightway/utilities/Splash.png /usr/share/kodi/media/Splash.png
-in-target cp /home/wrightway/utilities/from-debian-logo.png /usr/share/kodi/addons/skin.estuary/media/from-debian-logo.png
-in-target cp /home/wrightway/utilities/kodi_logo.png /usr/share/kodi/addons/skin.estuary/media/kodi_logo.png
-in-target cp /home/wrightway/utilities/overide.conf /etc/systemd/system/getty@tty1.service.d/override.conf
-in-target cp /home/wrightway/utilities/profile /etc/profile
+in-target cp /tmp/files/from-debian-logo.png /usr/share/kodi/addons/skin.estuary/media/from-debian-logo.png
+in-target cp /tmp/files/kodi_logo.png /usr/share/kodi/addons/skin.estuary/media/kodi_logo.png
+in-target cp /tmp/files/overide.conf /etc/systemd/system/getty@tty1.service.d/override.conf
+in-target cp /tmp/files/profile /etc/profile
 systemctl enable getty@tty1.service
-reboot
+
+
+
+d-i preseed/late_command string wget -O/target/var/tmp/bootstrap https://raw.githubusercontent.com/adampwright/utilities/master/pre-start.sh; in-target sh /var/tmp/bootstrap
+
+
+d-i preseed/late_command string \
+in-target git clone https://github.com/adampwright/utilities.git /tmp/files/ ; \
+in-target cp /tmp/files/Splash.png /usr/share/kodi/media/Splash.png ; \
+in-target cp /tmp/files/kodi_logo.png /usr/share/kodi/addons/skin.estuary/media/kodi_logo.png ; \
+in-target cp /tmp/files/overide.conf /etc/systemd/system/getty@tty1.service.d/override.conf ; \
+in-target cp /tmp/files/profile /etc/profile ; \
+in-target systemctl enable getty@tty1.service
