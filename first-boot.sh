5b327ed1e0adfef3ef51f0f410a9184c1d10d9af
adampwright <adam@wrightwaydigitalmedia.com>
2020-07-23 09:58:35 -0700

    tweaks

--- first-boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /etc/systemd/system/getty@.service.d/ && touch /etc/systemd/system/getty@.service.d/override.conf
-cp /home/wrightway/utilities/override.conf /etc/systemd/system/getty@.service.d/override.conf
-cp /home/wrightway/utilities/profile /etc/profile
-reboot
-rm -- "$0"
